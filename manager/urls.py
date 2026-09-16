4d2c80c90a9c95bb7a19ad07cebfffba2e5dbdbf
Yuri Castro Coelho <yuricastrocoelho90@gmail.com>
2026-09-16 01:32:49 -0300

    1 olhada do site

---
+++ manager/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        'produtos/',                
+        views.lista_produtos,       
+        name='lista_produtos'      
+    ),
+]
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Produto  
 
 # Create your views here.
+def lista_produtos(request):
+    
+    produtos = Produto.objects.all()
+    
+    
+    return render(
+        request, 
+        'manager/lista_produtos.html',  
+        {'produtos': produtos}       
+    )
