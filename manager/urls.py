395714f4c7bcc75142898d5c11f8feedc88dcf8b
Yuri <yuricastrocoelho90@gmail.com>
2026-09-21 20:50:42 -0300

    Parte fazendo

--- manager/urls.py
+++ manager/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
         views.lista_produtos,       
         name='lista_produtos'      
     ),
+    path(
+        'produtos/<int:produto_id>/adicionar/',
+        views.adicionar_ao_carrinho,
+        name = 'adicionar_carrinho'
+    )
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Produto  
+from .models import Produto , Cliente
 
 # Create your views here.
 def lista_produtos(request):
@@ -12,3 +12,14 @@ def lista_produtos(request):
         'manager/lista_produtos.html',  
         {'produtos': produtos}       
     )
+
+def adicionar_ao_carrinho(request, produto_id):
+
+    if request.method == 'POST':
+
+        produto = Produto.objects.get(id=produto_id)
+
+        valor_do_campo = request.POST.get('cliente_username')
+
+        cliente, foi_criado = Cliente.objects.get_or_create(nome=valor_do_campo)
+
